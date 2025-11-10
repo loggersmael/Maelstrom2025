3fe97dc5fdc9ced55ef3b019ee3fb2744537fc5d
loggersmael <srijankumbam@gmail.com>
2025-11-10 13:06:37 -0500

    shooterworks

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Test2.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="test2")
+public class Test2 extends OpMode
+{
+    private DcMotor intake;
+    private DcMotor shooter;
+    @Override
+    public void init()
+    {        intake= hardwareMap.get(DcMotor.class,"intake");
+        shooter= hardwareMap.get(DcMotor.class,"shooter");
+    }
+    @Override
+    public void loop()
+    {
+        if(gamepad1.a)
+        {
+            intake.setPower(-1);
+        }
+        else {
+            intake.setPower(0);
+        }
+        if(gamepad1.b)
+        {
+            intake.setPower(1);
+        }
+        else {
+            intake.setPower(0);
+        }
+        if(gamepad1.right_bumper)
+        {
+            shooter.setPower(1);
+        }
+        else {
+            shooter.setPower(0);
+        }
+        if(gamepad1.left_bumper)
+        {
+            shooter.setPower(-1);
+        }
+        else {
+            shooter.setPower(0);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -23,10 +25,22 @@ public class Constants {
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("frontRight")
+            .rightRearMotorName("backRight")
+            .leftRearMotorName("backLeft")
+            .leftFrontMotorName("frontLeft")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
+
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pinpointLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
