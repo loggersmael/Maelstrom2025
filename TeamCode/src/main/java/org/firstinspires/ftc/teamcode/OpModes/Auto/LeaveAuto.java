83138f7050fc162050d72d0512b069758cad741c
loggersmael <srijankumbam@gmail.com>
2025-11-15 08:50:42 -0500

    Create LeaveAuto.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/LeaveAuto.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.OpModes.Auto;
+
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Maelstrom;
+
+@Autonomous(name="LeaveAuto")
+public class LeaveAuto extends OpMode
+{
+
+    private Maelstrom robot;
+    private Timer time;
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+
+
+    @Override
+    public void init()
+    {
+       // robot= new Maelstrom(hardwareMap, telemetry, Maelstrom.Alliance.BLUE, gamepad1, gamepad2);
+       // time= new Timer();
+        //robot.dt.enableTeleop();
+        frontLeft= hardwareMap.get(DcMotor.class,"frontLeft");
+        frontRight= hardwareMap.get(DcMotor.class,"frontRight");
+        backLeft= hardwareMap.get(DcMotor.class,"backLeft");
+        backRight= hardwareMap.get(DcMotor.class, "backRight");
+    }
+
+    @Override
+    public void start()
+    {
+        time.resetTimer();
+        frontLeft.setPower(1);
+        frontRight.setPower(1);
+        backLeft.setPower(1);
+        backRight.setPower(1);
+        //robot.dt.setMovementVectors(0,1,0,false);
+    }
+
+    @Override
+    public void loop()
+    {
+        if(time.getElapsedTimeSeconds()>1)
+        {
+            //robot.dt.setMovementVectors(0,0,0,false);
+        }
+    }
+
+}
