e2da06f24fd622d435ea31311b17f9bd5766ea27
loggersmael <srijankumbam@gmail.com>
2025-11-15 19:51:59 -0500

    flywheel fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Maelstrom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Maelstrom.java
@@ -73,7 +73,11 @@ public class Maelstrom
 
         if(driver2.getButton(GamepadKeys.Button.RIGHT_BUMPER))
         {
-            shooter.toggleFlywheel();
+            shooter.flywheelOn=true;
+        }
+        if(driver2.getButton(GamepadKeys.Button.LEFT_BUMPER))
+        {
+            shooter.flywheelOn=false;
         }
 
         if(driver2.getButton(GamepadKeys.Button.Y))
@@ -171,6 +175,7 @@ public class Maelstrom
                     intake.stop();
                     setTransferState(3);
                 }
+                tTimer.resetTimer();
                 break;
             case 3:
                 if(tTimer.getElapsedTimeSeconds()>=0.05 && shooter.atSpeed())
@@ -178,6 +183,7 @@ public class Maelstrom
                     intake.kickerUp();
                     setTransferState(4);
                 }
+                tTimer.resetTimer();
                 break;
             case 4:
                 if(tTimer.getElapsedTimeSeconds()>=0.05)
@@ -185,6 +191,7 @@ public class Maelstrom
                     intake.kickerDown();
                     setTransferState(5);
                 }
+                tTimer.resetTimer();
                 break;
             case 5:
                 if(tTimer.getElapsedTimeSeconds()>=0.1)
@@ -192,6 +199,7 @@ public class Maelstrom
                     intake.spinIn();
                     setTransferState(6);
                 }
+                tTimer.resetTimer();
                 break;
             case 6:
                 if(tTimer.getElapsedTimeSeconds()>=0.1 && shooter.atSpeed())
@@ -199,6 +207,7 @@ public class Maelstrom
                     intake.kickerUp();
                     setTransferState(7);
                 }
+                tTimer.resetTimer();
                 break;
             case 7:
                 if(tTimer.getElapsedTimeSeconds()>=0.05)
@@ -207,6 +216,7 @@ public class Maelstrom
                     intake.stop();
                     setTransferState(-1);
                 }
+                tTimer.resetTimer();
                 break;
         }
 
