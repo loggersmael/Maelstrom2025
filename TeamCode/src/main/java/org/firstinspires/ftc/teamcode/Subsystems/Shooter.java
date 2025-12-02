2d6870d3e9c92d06c3bfc26cf038882743a85c0f
loggersmael <srijankumbam@gmail.com>
2025-12-01 20:46:05 -0500

    added LUT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import static org.firstinspires.ftc.teamcode.Utilities.Constants.ShooterConstants.closeVelocity;
+import static org.firstinspires.ftc.teamcode.Utilities.Constants.ShooterConstants.farVelocity;
 import static org.firstinspires.ftc.teamcode.Utilities.Constants.ShooterConstants.kD;
 import static org.firstinspires.ftc.teamcode.Utilities.Constants.ShooterConstants.kI;
 import static org.firstinspires.ftc.teamcode.Utilities.Constants.ShooterConstants.kP;
@@ -13,6 +15,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.seattlesolvers.solverslib.controller.PIDFController;
+import com.seattlesolvers.solverslib.util.InterpLUT;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Subsystems.Maelstrom;
@@ -22,12 +25,15 @@ public class Shooter extends SubsystemBase {
     public DcMotorEx shooterMotor;
     private Servo hoodServo;
     private Servo light;
-    private Limelight3A cam;
+    private Vision cam;
+    private InterpLUT table;
     private Maelstrom.Alliance alliance;
     private Telemetry telemetry;
     public double currentVelocity;
     public double targetVelocity;
+    public double autoVelocity;
     public boolean flywheelOn;
+    private double distance;
     public PIDFController velocityController= new PIDFController(kP,kI,kD,ShooterConstants.kF);
 
     public Shooter(HardwareMap aHardwareMap, Telemetry telemetry, Maelstrom.Alliance color) {
@@ -43,7 +49,14 @@ public class Shooter extends SubsystemBase {
         hoodServo.setPosition(0);
 
         light = aHardwareMap.get(Servo.class, "light");
-        cam = aHardwareMap.get(Limelight3A.class, "limelight");
+        cam = new Vision(aHardwareMap,telemetry,color);
+        distance=0;
+        autoVelocity=0;
+        table= new InterpLUT();
+        table.add(0,closeVelocity);
+        table.add(0,midVelocity);
+        table.add(0,farVelocity);
+        table.createLUT();
 
         flywheelOn = false;
         targetVelocity = ShooterConstants.closeVelocity; // start with some default
@@ -51,6 +64,8 @@ public class Shooter extends SubsystemBase {
 
     @Override
     public void periodic() {
+        updateDistance();
+
         // Update shooter velocity
         if (flywheelOn) {
             shooterMotor.setPower(velocityController.calculate(currentVelocity,targetVelocity));
@@ -83,6 +98,11 @@ public class Shooter extends SubsystemBase {
     public void shootFarAuto()
     {
         targetVelocity= 2000;
+    }
+
+    public void shootAutoVelocity()
+    {
+        targetVelocity=autoVelocity;
     }
 
     public void reverseWheel() {
@@ -124,6 +144,14 @@ public class Shooter extends SubsystemBase {
         return Math.abs(currentVelocity - targetVelocity) < velocityTolerance;
     }
 
+    public void updateDistance()
+    {
+        distance= cam.calcDistance();
+    }
+    public void updateAutoVelocity()
+    {
+        autoVelocity=table.get(distance);
+    }
     public void hoodUp()
     {
         hoodServo.setPosition(0.5);
